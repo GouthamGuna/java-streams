74338d5bf830c6e290bceea2cd2f588ee6dc0199
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-31 08:05:09 +0530

    initial commit

---
+++ src/main/java/in/dev/gmsk/predicate/PredicateInterfaceExampleOne.java
@@ -0,0 +1,20 @@
+package main.predicate;
+
+import java.util.function.Predicate;
+
+public class PredicateInterfaceExampleOne {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println( validateIndiaVoterAge( Integer.parseInt( args[0] ) ) );
+        } else {
+            System.out.println( "No command line arguments were provided." );
+            System.exit( 0 );
+        }
+    }
+
+    static String validateIndiaVoterAge(int age) {
+        Predicate<Integer> result = i -> (age >= 18);
+        return result.test( age ) ? "Your eligible to vote :-) " : "Sorry your not eligible :-( ";
+    }
+}
