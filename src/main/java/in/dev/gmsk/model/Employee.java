f4ef270d00b2a3d473c73c936ef1d7f9c6b68ca0
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-10 14:19:57 +0530

    Update Employee.java

--- src/main/java/in/dev/gmsk/model/Employee.java
+++ src/main/java/in/dev/gmsk/model/Employee.java
@@ -1,2 +1,13 @@
-package in.dev.gmsk.model;public class Employee {
+package in.dev.gmsk.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+
+    private String id;
+    private String name;
+    private String emailId;
 }
