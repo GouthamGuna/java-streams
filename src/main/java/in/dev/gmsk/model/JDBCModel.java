c7f7e74b0dad6df151a022a61357db13c038861e
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-10 13:17:19 +0530

    Update JDBCModel.java

--- src/main/java/in/dev/gmsk/model/JDBCModel.java
+++ src/main/java/in/dev/gmsk/model/JDBCModel.java
@@ -1,2 +1,20 @@
-package in.dev.gmsk.model;public class JDBCModel {
+package in.dev.gmsk.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class JDBCModel {
+
+    private String driverName;
+    private String protocol;
+    private String host;
+    private String port;
+    private String dbName;
+    private String dbUserName;
+    private String dbPassword;
+    private String URI;
 }
