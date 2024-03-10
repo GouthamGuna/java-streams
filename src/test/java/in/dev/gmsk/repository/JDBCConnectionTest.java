18c016a1322f1b059a45f2a7d16be6998f883aee
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-10 13:18:22 +0530

    Udpate JDBCConnectionTest.java;

--- src/test/java/in/dev/gmsk/repository/JDBCConnectionTest.java
+++ src/test/java/in/dev/gmsk/repository/JDBCConnectionTest.java
@@ -1,4 +1,27 @@
+package in.dev.gmsk.repository;
+
+import in.dev.gmsk.model.JDBCModel;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.BooleanSupplier;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class JDBCConnectionTest {
-  
+
+    @Test
+    void getConnection() throws SQLException {
+        JDBCModel dataBaseSession = JDBCConnection.getDataBaseSession();
+
+        assertNotNull( dataBaseSession );
+
+        Connection connection = JDBCConnection.getConnection( dataBaseSession );
+
+        assertTrue( connection.isValid( 0 ) );
+
+        connection.close();
+    }
 }
