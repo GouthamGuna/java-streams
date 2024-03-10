965b7f9df3b396f8cdff99b307c3538b7f85b8c0
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-10 14:20:41 +0530

    Update Simple.java

--- src/main/java/in/dev/gmsk/supplier/Simple.java
+++ src/main/java/in/dev/gmsk/supplier/Simple.java
@@ -1,2 +1,22 @@
-package in.dev.gmsk.supplier;public class Simple {
+package in.dev.gmsk.supplier;
+
+import in.dev.gmsk.model.Student;
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFirstName( null );
+
+        createAccount( student, () -> "Invalid name " + LocalDateTime.now() );
+    }
+
+    private static void createAccount(Student s, Supplier<String> errorMsg) {
+        if (s.getFirstName() == null) {
+            System.out.println( "errorMsg = " + errorMsg.get() );
+        }
+    }
 }
