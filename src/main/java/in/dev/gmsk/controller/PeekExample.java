7923fc07b374e87693e228f67da8acbbd5400647
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-13 08:09:02 +0530

    Update PeekExample.java

--- src/main/java/in/dev/gmsk/controller/PeekExample.java
+++ src/main/java/in/dev/gmsk/controller/PeekExample.java
@@ -1,2 +1,19 @@
-package in.dev.gmsk.controller;public class PeekExample {
+package in.dev.gmsk.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Peek method is used to print each element before it's multiplied by 2.
+ */
+public class PeekExample {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList( 1, 2, 3, 4, 5, 6, 7 );
+
+        numbers.stream()
+                .peek( num -> System.out.println( "Processing : " + num ) )
+                .map( n -> n * 2 )
+                .forEach( System.out::println );
+    }
 }
