f5c4ee4b6154bcb1ec8b7f93ae5b692f3fe7919e
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-14 22:03:09 +0530

    Update FeeCollectionListRefactor.java

--- src/main/java/in/dev/gmsk/function/FeeCollectionListRefactor.java
+++ src/main/java/in/dev/gmsk/function/FeeCollectionListRefactor.java
@@ -4,16 +4,15 @@ import in.dev.gmsk.model.Fee;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FeeCollectionListRefactor {
 
-    public static Fee getFee(){
+    public static Fee getFee() {
 
         Fee returnObj = new Fee();
         List<Fee> list = new ArrayList<>();
 
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
 
             Fee fee = new Fee();
             fee.setTotalAmount( "2000" );
@@ -24,24 +23,30 @@ public class FeeCollectionListRefactor {
         }
 
         returnObj.setTotalFeeAmount( "3000" );
-        returnObj.setFeeList(list );
+        returnObj.setFeeList( list );
 
         return returnObj;
     }
 
-    public static void runner(){
+    public static void runner() {
 
         Fee noOfObj = getFee();
+        Fee returnObj = new Fee();
 
-        double totalBalance = 0.0;
-
-        for(Fee obj: noOfObj.getFeeList()){
+        /*for(Fee obj: noOfObj.getFeeList()){
             totalBalance += Double.parseDouble( obj.getTotalBalance() );
+        }*/
+
+        double totalBalance = noOfObj.getFeeList() // List<Fee>
+                .parallelStream() // Stream<Fee>
+                .mapToDouble( obj -> Double.parseDouble( obj.getTotalBalance() ) ) // DoubleStream
+                .sum();
+
+        if (totalBalance != 0.0) {
+            returnObj.setTotalBalance( String.valueOf( totalBalance ) );
         }
 
-        if(totalBalance != 0.0){
-            System.out.println(noOfObj.getFeeList());
-        }
+
     }
 
     public static void main(String[] args) {
