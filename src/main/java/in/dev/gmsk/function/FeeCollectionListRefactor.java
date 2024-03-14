797eb0f2f8866b17cff498dc5b66d064cdc1d6cf
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-14 14:30:29 +0530

    Update .java

--- src/main/java/in/dev/gmsk/function/FeeCollectionListRefactor.java
+++ src/main/java/in/dev/gmsk/function/FeeCollectionListRefactor.java
@@ -1,2 +1,50 @@
-package in.dev.gmsk.function;public class FeeCollectionListRefactor {
+package in.dev.gmsk.function;
+
+import in.dev.gmsk.model.Fee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FeeCollectionListRefactor {
+
+    public static Fee getFee(){
+
+        Fee returnObj = new Fee();
+        List<Fee> list = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+
+            Fee fee = new Fee();
+            fee.setTotalAmount( "2000" );
+            fee.setTotalBalance( "100" );
+            fee.setTotalFeeAmount( "2000" );
+
+            list.add( fee );
+        }
+
+        returnObj.setTotalFeeAmount( "3000" );
+        returnObj.setFeeList(list );
+
+        return returnObj;
+    }
+
+    public static void runner(){
+
+        Fee noOfObj = getFee();
+
+        double totalBalance = 0.0;
+
+        for(Fee obj: noOfObj.getFeeList()){
+            totalBalance += Double.parseDouble( obj.getTotalBalance() );
+        }
+
+        if(totalBalance != 0.0){
+            System.out.println(noOfObj.getFeeList());
+        }
+    }
+
+    public static void main(String[] args) {
+        runner();
+    }
 }
--- src/main/java/in/dev/gmsk/model/Fee.java
+++ src/main/java/in/dev/gmsk/model/Fee.java
@@ -1,2 +1,53 @@
-package in.dev.gmsk.model;public class Fee {
+package in.dev.gmsk.model;
+
+import java.util.List;
+
+public class Fee {
+
+    private String totalAmount;
+    private String totalBalance;
+    private String totalFeeAmount;
+    private List<Fee> feeList;
+
+    public String getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void setTotalBalance(String totalBalance) {
+        this.totalBalance = totalBalance;
+    }
+
+    public String getTotalFeeAmount() {
+        return totalFeeAmount;
+    }
+
+    public void setTotalFeeAmount(String totalFeeAmount) {
+        this.totalFeeAmount = totalFeeAmount;
+    }
+
+    public List<Fee> getFeeList() {
+        return feeList;
+    }
+
+    public void setFeeList(List<Fee> feeList) {
+        this.feeList = feeList;
+    }
+
+    @Override
+    public String toString() {
+        return "Fee{" +
+                "totalAmount='" + totalAmount + '\'' +
+                ", totalBalance='" + totalBalance + '\'' +
+                ", totalFeeAmount='" + totalFeeAmount + '\'' +
+                ", feeList=" + feeList +
+                '}';
+    }
 }
