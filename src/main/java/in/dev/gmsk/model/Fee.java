53ec394464a959ed2bee93e3d8abfc2d04304534
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-14 22:03:27 +0530

    Update Fee.java

--- src/main/java/in/dev/gmsk/model/Fee.java
+++ src/main/java/in/dev/gmsk/model/Fee.java
@@ -1,53 +1,18 @@
 package in.dev.gmsk.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Data
+@Setter
+@Getter
 public class Fee {
 
     private String totalAmount;
     private String totalBalance;
     private String totalFeeAmount;
     private List<Fee> feeList;
-
-    public String getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(String totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public String getTotalBalance() {
-        return totalBalance;
-    }
-
-    public void setTotalBalance(String totalBalance) {
-        this.totalBalance = totalBalance;
-    }
-
-    public String getTotalFeeAmount() {
-        return totalFeeAmount;
-    }
-
-    public void setTotalFeeAmount(String totalFeeAmount) {
-        this.totalFeeAmount = totalFeeAmount;
-    }
-
-    public List<Fee> getFeeList() {
-        return feeList;
-    }
-
-    public void setFeeList(List<Fee> feeList) {
-        this.feeList = feeList;
-    }
-
-    @Override
-    public String toString() {
-        return "Fee{" +
-                "totalAmount='" + totalAmount + '\'' +
-                ", totalBalance='" + totalBalance + '\'' +
-                ", totalFeeAmount='" + totalFeeAmount + '\'' +
-                ", feeList=" + feeList +
-                '}';
-    }
 }
