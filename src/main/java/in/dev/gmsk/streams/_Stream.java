9e38a957b510891d3299021c10dafc2666cfe73b
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-24 12:11:20 +0530

    Update _Stream.java

---
+++ src/main/java/in/dev/gmsk/streams/_Stream.java
@@ -0,0 +1,42 @@
+package in.dev.gmsk.streams;
+
+import static in.dev.gmsk.util.Price.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class _Stream {
+
+    public static void main(String[] args) {
+
+        // prices greater than $20, discounted by 10%.
+        DiscountImperative(getPricesForSupplier.get());
+        DiscountDeclarative(getPrices());
+    }
+
+    /* Declarative style */
+
+    public static void DiscountDeclarative(List<BigDecimal> prices) {
+        final BigDecimal totalOfDiscountedPrices =
+                prices.stream()
+                        .filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+                        .map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+    }
+
+    /* Imperative style */
+
+    public static void DiscountImperative(List<BigDecimal> prices) {
+
+        BigDecimal totalOfDiscountedPrices = BigDecimal.ZERO;
+
+        for (BigDecimal price : prices) {
+            if (price.compareTo(BigDecimal.valueOf(20)) > 0)
+                totalOfDiscountedPrices =
+                        totalOfDiscountedPrices.add(price.multiply(BigDecimal.valueOf(0.9)));
+        }
+
+        System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+    }
+}
