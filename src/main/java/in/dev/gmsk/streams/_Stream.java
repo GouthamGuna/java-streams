ff01fcf1cc5e2b3eae04af882fddaf20ee9aa5a2
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-25 22:33:30 +0530

    initial commit

--- src/main/java/in/dev/gmsk/streams/_Stream.java
+++ src/main/java/in/dev/gmsk/streams/_Stream.java
@@ -32,9 +32,10 @@ public class _Stream {
         BigDecimal totalOfDiscountedPrices = BigDecimal.ZERO;
 
         for (BigDecimal price : prices) {
-            if (price.compareTo(BigDecimal.valueOf(20)) > 0)
+            if (price.compareTo(BigDecimal.valueOf(20)) > 0) {
                 totalOfDiscountedPrices =
                         totalOfDiscountedPrices.add(price.multiply(BigDecimal.valueOf(0.9)));
+            }
         }
 
         System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
