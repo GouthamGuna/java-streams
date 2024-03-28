9b8a0d528c6eb708dd5a2a8bba4460e9e14c70ad
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-29 02:58:47 +0530

    Update _Optional.java

--- src/main/java/in/dev/gmsk/optionals/_Optional.java
+++ src/main/java/in/dev/gmsk/optionals/_Optional.java
@@ -1,2 +1,14 @@
-package in.dev.gmsk.optionals;public class _Optional {
+package in.dev.gmsk.optionals;
+
+import java.util.Optional;
+
+public class _Optional {
+    public static void main(String[] args) {
+
+        Optional.ofNullable(null)
+                .ifPresentOrElse(
+                        email -> System.out.println("Sending email to " + email),
+                        () -> System.out.println("Cannot send email"));
+
+    }
 }
