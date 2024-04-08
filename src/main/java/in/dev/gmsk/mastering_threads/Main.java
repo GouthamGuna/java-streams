c63809f3e009b892837723f43afe4b131b23e264
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-08 22:36:55 +0530

    initial commit

---
+++ src/main/java/in/dev/gmsk/mastering_threads/Main.java
@@ -0,0 +1,11 @@
+package in.dev.gmsk.mastering_threads;
+
+import static in.dev.gmsk.mastering_threads.JavaConcurrency.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        webServer();
+        photoEditor();
+    }
+}
