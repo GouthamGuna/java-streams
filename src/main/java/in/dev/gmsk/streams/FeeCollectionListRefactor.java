9ba23c29447cb4295687286b2cf6d9c81280e5b5
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-10 06:40:59 +0530

    Update /FeeCollectionListRefactor.java

--- src/main/java/in/dev/gmsk/streams/FeeCollectionListRefactor.java
+++ src/main/java/in/dev/gmsk/streams/FeeCollectionListRefactor.java
@@ -3,7 +3,9 @@ package in.dev.gmsk.streams;
 import lombok.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -13,36 +15,11 @@ import static java.lang.System.*;
 public class FeeCollectionListRefactor {
 
     public static void main(String[] args) {
-
-        List<Fee> mfqFeesWithoutHostel = filterMFQFeeWithOutHostel.apply(getStudent.get());
-        List<Fee> mfqFeesWithHostel = filterMFQFeeWithHostel.apply(getStudent.get());
-        List<Fee> gfqFeesWithoutHostel = filterGFQFeeWithOutHostel.apply(getStudent.get());
-        List<Fee> gfqFeesWithHostel = filterGFQFeeWithHostel.apply(getStudent.get());
-
-        ConcurrentHashMap<String, List<Fee>> result = new ConcurrentHashMap<>();
-        result.put("mfqFeesWithoutHostel", filterMFQFeeWithOutHostel.apply(getStudent.get()));
-        result.put("mfqFeesWithHostel", filterMFQFeeWithHostel.apply(getStudent.get()));
-        result.put("gfqFeesWithoutHostel", filterGFQFeeWithOutHostel.apply(getStudent.get()));
-        result.put("gfqFeesWithHostel", filterGFQFeeWithHostel.apply(getStudent.get()));
-
-        if (getStudent.get().getFeeType().equalsIgnoreCase("GFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("NO")) {
-            out.println("\nResult : " + result.get("gfqFeesWithoutHostel"));
-        } else if (getStudent.get().getFeeType().equalsIgnoreCase("GFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("Yes")) {
-            out.println("\nResult : " + result.get("gfqFeesWithHostel"));
-        } else if (getStudent.get().getFeeType().equalsIgnoreCase("MFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("No")) {
-            out.println("\nResult : " + result.get("mfqFeesWithoutHostel"));
-        } else if (getStudent.get().getFeeType().equalsIgnoreCase("MFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("Yes")) {
-            out.println("\nResult : " + result.get("mfqFeesWithHostel"));
-        }
-
-        out.println("MFQ Fees without Hostel: " + mfqFeesWithoutHostel);
-        out.println("MFQ Fees with Hostel: " + mfqFeesWithHostel);
-        out.println("GFQ Fees without Hostel: " + gfqFeesWithoutHostel);
-        out.println("GFQ Fees with Hostel: " + gfqFeesWithHostel);
+        finalTestingScenario.accept(getStudent.get());
     }
 
     private static final Supplier<Student> getStudent = () ->
-            new Student("Sathaya", "mfq", "null");
+            new Student("Sathaya", "gfq", "no");
 
     private static final Supplier<Stream<Fee>> getFeeList = () -> Stream.of(
             new Fee("GFQ", 8000),
@@ -67,6 +44,45 @@ public class FeeCollectionListRefactor {
             .filter(f -> !f.getFeeTypeId().equalsIgnoreCase("MFQ"))
             .toList();
 
+    private static void testingScenario() {
+
+        List<Fee> mfqFeesWithoutHostel = filterMFQFeeWithOutHostel.apply(getStudent.get());
+        List<Fee> mfqFeesWithHostel = filterMFQFeeWithHostel.apply(getStudent.get());
+        List<Fee> gfqFeesWithoutHostel = filterGFQFeeWithOutHostel.apply(getStudent.get());
+        List<Fee> gfqFeesWithHostel = filterGFQFeeWithHostel.apply(getStudent.get());
+
+        err.println("\nMFQ Fees without Hostel: " + mfqFeesWithoutHostel);
+        err.println("\nMFQ Fees with Hostel: " + mfqFeesWithHostel);
+        err.println("\nGFQ Fees without Hostel: " + gfqFeesWithoutHostel);
+        err.println("\nGFQ Fees with Hostel: " + gfqFeesWithHostel);
+    }
+
+    private static void testingScenarioOne() {
+
+        if (getStudent.get().getFeeType().equalsIgnoreCase("GFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("NO")) {
+            out.println("\nResult : " + filterGFQFeeWithOutHostel.apply(getStudent.get()));
+        } else if (getStudent.get().getFeeType().equalsIgnoreCase("GFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("Yes")) {
+            out.println("\nResult : " + filterGFQFeeWithHostel.apply(getStudent.get()));
+        } else if (getStudent.get().getFeeType().equalsIgnoreCase("MFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("No")) {
+            out.println("\nResult : " + filterMFQFeeWithOutHostel.apply(getStudent.get()));
+        } else if (getStudent.get().getFeeType().equalsIgnoreCase("MFQ") && getStudent.get().getIsHostel().equalsIgnoreCase("Yes")) {
+            out.println("\nResult : " + filterMFQFeeWithHostel.apply(getStudent.get()));
+        }
+    }
+
+    private static final Consumer<Student> finalTestingScenario = s -> {
+
+        ConcurrentHashMap<List<Fee>, Boolean> conduction = new ConcurrentHashMap<>();
+        conduction.put(filterGFQFeeWithOutHostel.apply(s), s.getFeeType().equalsIgnoreCase("GFQ") && s.getIsHostel().equalsIgnoreCase("NO"));
+        conduction.put(filterGFQFeeWithHostel.apply(s), s.getFeeType().equalsIgnoreCase("GFQ") && s.getIsHostel().equalsIgnoreCase("Yes"));
+        conduction.put(filterMFQFeeWithOutHostel.apply(s), s.getFeeType().equalsIgnoreCase("MFQ") && s.getIsHostel().equalsIgnoreCase("No"));
+        conduction.put(filterMFQFeeWithHostel.apply(s), s.getFeeType().equalsIgnoreCase("MFQ") && s.getIsHostel().equalsIgnoreCase("Yes"));
+
+
+        Stream<Map.Entry<List<Fee>, Boolean>> stream = conduction.entrySet().parallelStream();
+
+        stream.filter(Map.Entry::getValue).map(k -> "\nResult : " + k.getKey()).forEach(out::println);
+    };
 }
 
 @Getter
